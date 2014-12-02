8ea136e86dc20742fbb9e5de458f070c43f4db69
MOHAMED MUSSE <mahadahmed@mahads-MacBook-Pro.local>
2014-12-01 18:51:54 -0800

    break_me.py

--- Students/MohamedMusse/break_me.py
+++ Students/MohamedMusse/break_me.py
@@ -1,3 +1,17 @@
 def superman():
     print "I am superman"
+
+def name_error():
+     print a
+     
+def syntax_error():
+	a = 5
+	b = 10
+	 print a + c
+
+
+def attribute_error():
+	int.uppercase
+	
+			
     
